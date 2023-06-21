20b3e351813e6d50529a07851d3f3c25ffcf51d0
jayeshr28 <jayeshrathore2810@gmail.com>
2023-06-21 17:35:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import AgeCalculator from './components/Task5';
+
+function App() {
+  return (
+    <>
+    
+    <div className='center'>
+      <h1>Age Calculator</h1>
+      <AgeCalculator />
+  </div>
+    </>
+    
+  );
+}
+
+export default App;
